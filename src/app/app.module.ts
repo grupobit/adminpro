c5a061e03f8690b3407d31c90b18b7e7c4e4b2bc
Mairon Cuello <soportesistemasit@outlook.com>
2018-10-17 16:13:06 -0500

    Modificaciones en los Modulo y Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,15 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTES } from './app.routes'; 
 
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+  // Importamos el archivo pages.module.ts donde se declaro DashboardComponent, ProgressComponent, Graficas1Component, PagesComponent
+import { SharedModule } from './shared/shared.module';
+// Importamos el archivo pages.module.ts donde se declaro DashboardComponent, ProgressComponent, Graficas1Component
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -23,19 +22,12 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
